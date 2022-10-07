b02dc43dc6da9ed90aff519e5d13f09fad1ea9f3
Folasayo Samuel <folasayosamuel@gmail.com>
2022-10-07 19:58:59 +0100

    update

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+using System;
+
+namespace CountingTotalLetterInText
+{
+	public class Program
+	{
+		public static void Main(string[]args)
+		{
+			Console.Write("Enter string: ");
+			var myString = Console.ReadLine();
+			var count = 0;
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            for (int i = 0; i < myString.Length; i++)
+			{
+				if(!char.IsWhiteSpace(myString[i]))
+					count++;
+			}
+			
+			Console.WriteLine("Total letters: "+ count);
+			Console.ReadLine();
+
+		}
+	}
+}
